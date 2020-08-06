851c3440b4b598ad087ce5f366f962aa4ae1a741
Dhinesh <sattia.dhinesh@gmail.com>
2020-08-06 20:07:54 +0200

    add delete

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList, Text} from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: 1, text: 'Hello'},
+    {id: 2, text: 'World'},
+  ]);
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id !== id);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Header title="Todolist" />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
     </View>
   );
 };
