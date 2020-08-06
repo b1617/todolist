9498e926895236da866b9440f8f835ee1fed03d6
Dhinesh <sattia.dhinesh@gmail.com>
2020-08-06 19:46:58 +0200

    add header

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+      <Header title="Todolist" />
     </View>
   );
 };
@@ -12,11 +13,6 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 30,
   },
 });
 
---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Header = ({title}) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+};
+
+Header.defaultProps = {
+  title: 'Todolist',
+};
+
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    padding: 15,
+    backgroundColor: 'darkblue',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 22,
+    textAlign: 'center',
+  },
+});
+
+export default Header;
